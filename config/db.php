6561041f346dc60af8cfaf817b8b2a3f9c944fe0
Asretdinova <54065122+Asretdinova@users.noreply.github.com>
2024-04-10 17:58:54 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tamojnyaDb',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=gd7sv4oce6abhut7;port=3306',
+    'username' => 'z4e5gd9jtsbsxvoi',
+    'password' => 'wgcr32gb2kyeul24',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
